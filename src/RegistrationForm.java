b3c4963a417aadd0347e33f473cf4c42e988df83
Fel-ly <kiokofpm@gmail.com>
2024-03-21 12:57:05 +0300

    registration form

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -1,2 +1,17 @@
-public class RegistrationForm {
+import javax.swing.*;
+
+public class RegistrationForm extends JDialog{
+    private JLabel RegisterPanel;
+    private JTextField tfName;
+    private JTextField tfEmail;
+    private JTextField tfPhone;
+    private JTextField pfPassword;
+    private JTextField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+
+    public RegistrationForm (JFrame parent){
+
+    }
+
 }
